0c4c983809c462dbe78b4232e2102fc4b1d73f35
xuanzhi33 <xuanzhi33@qq.com>
2026-01-17 23:32:24 +0000

    fix: update HomeView layout for consistent alignment of ServiceStatusCard

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,9 +4,9 @@ import ServiceStatusCard from '@/components/dashboard/ServiceStatusCard.vue';
 </script>
 
 <template>
-  <div class="p-10">
+  <div class="p-10 h-full flex items-center">
 
-    <ServiceStatusCard />
+    <ServiceStatusCard class="flex-1" />
 
   </div>
 </template>
